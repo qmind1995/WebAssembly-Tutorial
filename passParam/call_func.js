0a1e35bf9d24265eb8dbb99376ed40a2467e2e05
tritq2 <tritq2@vng.com.vn>
2018-01-24 10:26:55 +0700

    access mem

--- passParam/call_func.js
+++ passParam/call_func.js
@@ -1,40 +1,14 @@
-include('index.js');
-
-function call_AddnumCppFunc(){
-
-	const arrayDataToPass = [1.5, 2.2, 3.0, 4.3, 5.1]
-	let buffer
-	let error
-	let result
-
-	try {
-	    // Init the typed array with the same length as the number of items in the array parameter
-	    const typedArray = new Float32Array(arrayDataToPass.length)
-
-	    // Populate the array with the values
-	    for (let i=0; i<arrayDataToPass.length; i++) {
-	        typedArray[i] = arrayDataToPass[i]
-	    }
-
-	    // Allocate some space in the heap for the data (making sure to use the appropriate memory size of the elements)
-	    buffer = Module._malloc(typedArray.length * typedArray.BYTES_PER_ELEMENT)
-
-	    // Assign the data to the heap - Keep in mind bytes per element
-	    Module.HEAPF32.set(typedArray, buffer >> 2)
-
-	    // Finally, call the function with "number" parameter type for the array (the pointer), and an extra length parameter
-	    result = Module.__Z7addNumsPfi(buffer, arrayDataToPass.length);
-
-	} catch (e) {
-	    error = e
-	} finally {
-	    // To avoid memory leaks we need to always clear out the allocated heap data
-	    // This needs to happen in the finally block, otherwise thrown errors will stop code execution before this happens
-	    Module._free(buffer)
-	}
-
-	// Finally, throw any errors so that we know when something goes wrong
-	if (error) throw error
-
-	return result;
+function fetchAndInstantiate(url, importObject) {
+  return fetch(url).then(response =>
+    response.arrayBuffer()
+  ).then(bytes =>
+    WebAssembly.instantiate(bytes, importObject)
+  ).then(results =>
+    results.instance
+  );
 }
+
+fetchAndInstantiate("pass_param.wasm")
+	.then(function(instance) {
+        let ins = instance;
+    });
--- passParam/pass_param.cpp
+++ passParam/pass_param.cpp
@@ -12,7 +12,6 @@ int8_t EMSCRIPTEN_KEEPALIVE printIntParam(int8_t s) {
 // pass an array float
 float EMSCRIPTEN_KEEPALIVE addNums (float *buffer, int bufSize) {
     float total = 0;
-
     for (int i=0; i<bufSize; i++) {
         total+= buffer[i];
     }
@@ -20,6 +19,10 @@ float EMSCRIPTEN_KEEPALIVE addNums (float *buffer, int bufSize) {
     return total;
 }
 
+// pass an JS.Object
+uint8_t EMSCRIPTEN_KEEPALIVE  *addOneForAllAtts(uint8_t * objString){
+	return NULL;
+}
 
 int main(){
 	printIntParam(1);
